aa3796d6a31b8e541c357ae021a1d1a0ff1a7ceb
fukiran <macdorphoto@yahoo.com>
2021-02-24 16:00:52 +0000

    initial commit

---
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -0,0 +1,30 @@
+package com.codeclan.example.WhiskyTracker;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhiskyTrackerApplicationTests {
+	@Autowired
+	WhiskyRepository whiskyRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void canFindWhiskyByYear(){
+		List<Whisky> foundWhisky = whiskyRepository.findWhiskyByYear(1991);
+		assertEquals(1, foundWhisky.size());
+	}
+}
